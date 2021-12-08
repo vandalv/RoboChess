5a367015a8d757bf175edae10a83d9afb4970cf9
vandalv <64989065+vandalv@users.noreply.github.com>
2021-12-09 00:07:52 +0100

    ->

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
     public Unit selectedUnit;
 
     public int playerTurn = 1;
+    public Image playerIndicator;
+    public Sprite player1Indicator;
+    public Sprite player2Indicator;
+
 
     public void resetTiles()
     {
@@ -27,10 +32,12 @@ public class GameMaster : MonoBehaviour
         if(playerTurn == 1)
         {
             playerTurn = 2;
+            playerIndicator.sprite = player2Indicator;
         }
         else if(playerTurn == 2)
         {
             playerTurn = 1;
+            playerIndicator.sprite = player1Indicator;
         }
         if(selectedUnit != null)
         {
